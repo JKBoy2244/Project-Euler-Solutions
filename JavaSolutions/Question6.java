3e0837769107f2e11077e39b394f031744919464
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 17:40:31 +0000

    Create Question6.java

---
+++ JavaSolutions/Question6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int total1 = 0; //Initialises the 3 totals from total1 to total3
+    int total2 = 0;
+    int total3 = 0;
+
+    for (int i = 1; i <= 100; i++) {
+      total1 += (i*i); //Increments the initialising total1 by every single number's squares in the loop in order continuously
+    }
+
+    for (int j = 1; j <= 100; j++) {
+      total2 += j; //Increments the initialising total2 by every single number on the loop in order continuously
+      total3 = (total2*total2); //sqaures total2 to give the square of the sum of the first 100 numbers
+    }
+    System.out.println(total1); //prints total1
+    System.out.println(total3); //prints total3
+    System.out.println(total3-total1); //prints difference between total3 and total1
+  }
+}
