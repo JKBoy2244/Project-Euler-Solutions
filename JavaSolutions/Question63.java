7134650467ec0ffabba10458ed239838d397aead
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 18:06:17 +0000

    Create Question63.java

---
+++ JavaSolutions/Question63.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int count = 0;
+    
+    for (long i = 1; i < 1000000000L; i++) {
+
+      double value = Math.pow(i, 1.0 / String.valueOf(i).length());
+      if (Math.abs(value - Math.round(value)) < 1e-9) {
+        count++;
+       }
+      }
+
+    System.out.println(count);
+  }
+}
